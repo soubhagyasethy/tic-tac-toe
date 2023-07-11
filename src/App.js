ba4144f36796f7e37642152c2c3721286f64490f
soubhagyasethy <ssethy017@gmail.com>
2023-07-11 15:40:49 +0530

    building the board

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import "./App.css";
-
-function App() {
+export default function Square() {
   return (
-    <div className="App">
-      <h1>tic tac toe</h1>
-    </div>
+    <>
+      <div className="board-row">
+        <button className="square">1</button>
+        <button className="square">2</button>
+        <button className="square">3</button>
+      </div>
+      <div className="board-row">
+        <button className="square">4</button>
+        <button className="square">5</button>
+        <button className="square">6</button>
+      </div>
+      <div className="board-row">
+        <button className="square">7</button>
+        <button className="square">8</button>
+        <button className="square">9</button>
+      </div>
+    </>
   );
 }
-
-export default App;
